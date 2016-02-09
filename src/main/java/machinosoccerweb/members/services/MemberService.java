a2644c0bf2a452aa22f8d7b4487255c1f2c73e90
ryo-murai <murai.ry@gmail.com>
2016-02-09 20:29:53 +0900

    add member form

--- src/main/java/machinosoccerweb/members/services/MemberService.java
+++ src/main/java/machinosoccerweb/members/services/MemberService.java
@@ -1,9 +1,21 @@
 package machinosoccerweb.members.services;
 
+import machinosoccerweb.members.models.Member;
+import machinosoccerweb.members.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+  private final MemberRepository memberRepository;
 
-//  public
+  @Autowired
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  public Member save(Member member) {
+    // TODO: fill memberNo if it is blank (in case of adding a new member)
+    return memberRepository.save(member);
+  }
 }
---
+++ src/main/java/machinosoccerweb/members/web/MemberController.java
@@ -0,0 +1,110 @@
+package machinosoccerweb.members.web;
+
+import java.util.Date;
+import javax.persistence.Enumerated;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import machinosoccerweb.infra.validator.Katakana;
+import machinosoccerweb.members.models.Gender;
+import machinosoccerweb.members.models.Grade;
+import machinosoccerweb.members.models.Member;
+import machinosoccerweb.members.models.Parent;
+import machinosoccerweb.members.repositories.ParentRepository;
+import machinosoccerweb.members.services.MemberService;
+import machinosoccerweb.security.LoginUser;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Slf4j
+@Controller
+public class MemberController {
+  private final ParentRepository parentRepository;
+  private final MemberService memberService;
+
+  @Autowired
+  public MemberController(ParentRepository parentRepository, MemberService memberService) {
+    this.parentRepository = parentRepository;
+    this.memberService = memberService;
+  }
+
+  @RequestMapping(value = "/mypage/member/new", method = RequestMethod.GET)
+  public String memberForm(@AuthenticationPrincipal LoginUser loginUser,
+      @ModelAttribute("member") MemberForm memberForm) {
+
+    if (!loginUser.isParentRegistered()) {
+      // illegal window trantision
+      return "redirect:/contact";
+    }
+
+    Parent parent = parentRepository.findOne(loginUser.getFamilyId());
+    memberForm.setFamilyName(parent.getFamilyName());
+    memberForm.setFamilyNameKana(parent.getFamilyNameKana());
+
+    return "mypage/member";
+  }
+
+  @RequestMapping(value = "/mypage/member/new", method = RequestMethod.POST)
+  public String saveMember(@AuthenticationPrincipal LoginUser loginUser,
+      @Validated @ModelAttribute("member") MemberForm memberForm, BindingResult result) {
+
+    // TODO: validate joinedAt if it is in range from 2013-05 to today, inclusive.
+    //
+
+    if (result.hasErrors()) {
+      log.debug("validation error:{}", result);
+      return "mypage/member";
+    }
+
+    Member member = new Member();
+    BeanUtils.copyProperties(memberForm, member);
+
+    member.setFamilyId(loginUser.getFamilyId());
+    memberService.save(member);
+
+    return "redirect:/mypage";
+  }
+
+  @Data
+  private static class MemberForm {
+
+    private String memberNo;
+
+    private int userId;
+
+    @NotBlank
+    private String givenName;
+    @NotBlank
+    private String familyName;
+    @NotBlank
+    @Katakana
+    private String givenNameKana;
+    @NotBlank
+    @Katakana
+    private String familyNameKana;
+
+    @NotNull
+    @Enumerated
+    private Grade grade;
+
+    @NotNull
+    @Enumerated
+    private Gender gender;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM")
+    private Date joinedAt;
+
+  }
+}
