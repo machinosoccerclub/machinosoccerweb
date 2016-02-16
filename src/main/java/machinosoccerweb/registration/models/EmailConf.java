371b00b2a05aa1f77c06b2899d4411412bc6e77e
ryo-murai <murai.ry@gmail.com>
2016-02-16 14:45:16 +0900

    remove v1 migration and keep id as a serial type(not bigserial)

--- src/main/java/machinosoccerweb/registration/models/EmailConf.java
+++ src/main/java/machinosoccerweb/registration/models/EmailConf.java
@@ -23,7 +23,7 @@ public class EmailConf {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private Integer id;
 
   private String email;
 
--- src/main/java/machinosoccerweb/registration/repositories/EmailConfRepository.java
+++ src/main/java/machinosoccerweb/registration/repositories/EmailConfRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmailConfRepository extends JpaRepository<EmailConf, Long> {
+public interface EmailConfRepository extends JpaRepository<EmailConf, Integer> {
 }
