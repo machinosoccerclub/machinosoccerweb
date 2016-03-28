91ec2913ba8665018e82a95b463e910037ce38c0
ryo-murai <murai.ry@gmail.com>
2016-03-28 19:40:11 +0900

    migrate uploading by gdata api

--- src/main/java/machinosoccerweb/registration/web/UploadController.java
+++ src/main/java/machinosoccerweb/registration/web/UploadController.java
@@ -5,20 +5,19 @@ import java.io.InputStream;
 import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import machinosoccerweb.infra.google.GooglePicasa;
 import machinosoccerweb.members.models.Gender;
 import machinosoccerweb.members.models.Grade;
-import machinosoccerweb.members.models.Member;
 import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,9 +28,16 @@ import retrofit.RetrofitError;
 @Slf4j
 @Controller
 public class UploadController {
+  private final String attendUrl;
 
-  @Value("${machinosoccerweb.attendUrl}")
-  private String attendUrl;
+  private final GooglePicasa googlePicasa;
+
+  @Autowired
+  public UploadController(@Value("${machinosoccerweb.attendUrl}") String attendUrl,
+                          GooglePicasa googlePicasa) {
+    this.attendUrl = attendUrl;
+    this.googlePicasa = googlePicasa;
+  }
 
   @RequestMapping(value = "/completed", method = RequestMethod.GET)
   public String completed(Model model) {
@@ -40,39 +46,25 @@ public class UploadController {
   }
 
   @RequestMapping(value = "/upload", method = RequestMethod.GET)
-  public String uploadForm(@ModelAttribute("memberPhoto") MemberPhotoForm form) {
+  public String uploadForm() {
     return "registration/upload";
   }
 
-  @RequestMapping(value = "/upload2", method = RequestMethod.POST)
+  @RequestMapping(value = "/upload", method = RequestMethod.POST)
   public String upload(@RequestParam("name") String name,
                        @RequestParam("nameKana") String namekana,
                        @RequestParam("grade") String grade,
                        @RequestParam("gender") String gender,
                        @RequestParam("photo") MultipartFile file) throws IOException {
     if (!file.isEmpty()) {
-//      String nameAndKana = normalizeName(name, namekana);
-//      try (InputStream is = file.getInputStream()) {
-//        picasaweb.uploadPhoto(
-//            file.getOriginalFilename(),
-//            grade + "・" + nameAndKana,
-//            determineContentType(file),
-//            new String[]{grade, gender, genderTags.get(grade)},
-//            is);
-//      } catch (RetrofitError error) {  // todo: move to exception handler
-//        log.error("could not upload the photo." +
-//                "filename:{}, givenContentType:{}, memberName:{}, status:{}, reason:{}",
-//            file.getOriginalFilename(),
-//            file.getContentType(),
-//            Base64.getEncoder().encodeToString(nameAndKana.getBytes("utf8")),
-//            error.getResponse() != null ? error.getResponse().getStatus() : "null response",
-//            error.getResponse() != null ? error.getResponse().getReason() : "null response");
-//        log.error("picasa service replies ...: {}",
-//            error.getResponse() != null ? error.getResponse().getBody() : "null response");
-//
-//        // stack trace will be logged by exception handler
-//        throw error;
-//      }
+      String nameAndKana = normalizeName(name, namekana);
+      try (InputStream is = file.getInputStream()) {
+        googlePicasa.uploadPhoto(file.getOriginalFilename(),
+            grade + "・" + nameAndKana,
+            new String[]{grade, gender, genderTags.get(grade)},
+            is,
+            determineMediaType(file.getContentType(), file.getOriginalFilename()));
+      }
 
       return "redirect:/completed";
     } else {
@@ -83,42 +75,35 @@ public class UploadController {
     }
   }
 
-  @RequestMapping(value = "/upload", method = RequestMethod.POST)
-  public String upload(@Validated @ModelAttribute("memberPhoto") MemberPhotoForm form,
-                       BindingResult result) {
-    if(result.hasErrors()) {
-      return "registration/upload";
-    }
-    Member member = new Member();
-    BeanUtils.copyProperties(form, member);
+  private String determineMediaType(String uploadedContentType, String originalFilename) {
+    Supplier<String> mediaTypeFromFileExt = () ->
+        createMediaTypeFromFilename(originalFilename, uploadedContentType);
 
-    log.debug("upload requested with member:", member);
-
-    return "redirect:/completed";
+    return GooglePicasa.normalizeMediaType(uploadedContentType)
+        .orElseGet(mediaTypeFromFileExt);
   }
 
+  private String createMediaTypeFromFilename(String originalFilename, String uploadedContentType) {
+    Function<String, String> warnLog = (String mediaTypeToUse) -> {
+      log.warn("we are attempting upload the file to picasa:`{}` using mediaType: `{}`," +
+              " because we got unsupported content type from browser: `{}`",
+          originalFilename, mediaTypeToUse, uploadedContentType);
+      return mediaTypeToUse;
+    };
 
-  protected String determineContentType(MultipartFile file) {
-//    if (picasaweb.isSupportedMediaType(file.getContentType())) {
-//      return file.getContentType();
-//    } else {
-//      String mediaType = "image/" + getFileExt(file.getOriginalFilename());
-//      if (!picasaweb.isSupportedMediaType(mediaType)) {
-//        log.warn("we are trying to upload the photo to the GooglePicasa" +
-//            " with unsupported media type ... : {}", mediaType);
-//      }
-//      return mediaType;
-//    }
-    return "";
+    int indexOfExt = originalFilename.indexOf(".");
+    if (indexOfExt >= 0) {
+      String contentTypeByFileext = "image/" +
+          originalFilename.substring(indexOfExt + 1).toLowerCase();
+      return googlePicasa.normalizeMediaType(contentTypeByFileext)
+          .orElseGet(() -> warnLog.apply(contentTypeByFileext));
+    } else {
+      return warnLog.apply(uploadedContentType);
+    }
   }
 
   private String normalizeName(String name, String kana) {
     return (name.trim() + "（" + kana.trim() + "）").replace(' ', '　');
-  }
-
-  private String getFileExt(String filename) {
-    int i = filename.indexOf(".");
-    return i >= 0 ? filename.substring(i + 1) : "";
   }
 
   private static final Map<String, String> genderTags = new LinkedHashMap<String, String>() {
@@ -134,20 +119,4 @@ public class UploadController {
       put("小６", "MachisakaFC-U12");
     }
   };
-
-  @Data
-  private static class MemberPhotoForm {
-    @NotBlank
-    private String name;
-    @NotBlank
-    private String namekana;
-
-    @NotBlank
-    private Grade grade;
-    @NotBlank
-    private Gender gender;
-
-    @NotBlank
-    private MultipartFile file;
-  }
 }
